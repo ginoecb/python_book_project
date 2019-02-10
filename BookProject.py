3606a04b4ae3791a2ae89d4fe6c315f25c163f43
Gino Bryja <g3bryja@gmail.com>
2019-02-09 20:24:44 -0500

    wip

--- BookProject.py
+++ BookProject.py
@@ -12,9 +12,56 @@ def get_image():
     img = Image.open(img_name)
     img_la = img.convert('L')
     arr = np.asarray(img_la)
+    arr_rotated = np.transpose(arr)
     return arr
 
-def get_book_info():
+def get_num(message_str, min_int, max_int):
+    ''' Get a restricted numerical input from the user '''
+    num = None
+    while True:
+        data = input(message_str)
+        try:
+            num = float(data)
+            if num >= min_int and num <= max_int:
+                break
+            else:
+                print("ERROR: Input must be between " + str(min_int) + " and " + str(max_int))
+        except:
+            print("ERROR: Input must be a number\n")
+    return num
+
+def get_book_data():
+    img_data = get_image()
+    height = get_num("Enter book height in inches\n")
+    width = get_num("Enter book width in inches\n")
+    num_pages = get_num("Enter number of pages in book\n")
+    tolerance = get_num("Input a tolerance-threshold (0 - 255)\n"
+                      "All values below this will not be considered part of the image\n")
+    for i in img_data:
+        print("Page " + i)
+        get_page_cuts(img_data[1], tolerance)
+
+def get_page_cuts(arr, tolerance):
+    start = -1
+    stop = -1
+    cuts_list = []
+    cut = []
+    i = 0
+    for elt in arr:
+        if elt >= tolerance and start == -1:
+            start = i
+            cut.append(start)
+            stop = -1
+        elif elt < tolerance and start != -1 and stop == -1:
+            stop = i
+            cut.append(stop)
+            cuts_list.append(cut)
+            cut = []
+            start = -1
+        i += 1
+    return cuts_list
+
+
 
 """
 testarr = get_image()
